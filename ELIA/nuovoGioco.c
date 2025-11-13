3307ed622308f0ee755fc3afdb5231d096f8d8f4
elia cavasin <elia.cavasin2406@gmail.com>
2025-11-13 22:56:05 +0100

    sistema di print dei salvataggi
    aiuto ho fatto un casino non so manco cosa ho scritto ha fatto tutto gemini però funziona

--- ELIA/nuovoGioco.c
+++ ELIA/nuovoGioco.c
@@ -3,7 +3,7 @@
 #include "pulisciSchermo.h"
 #include "nuovoGioco.h"
 #include <time.h>
-#include "nuovoSalvataggio.h"
+#include "salvataggi.h"
 
 
 void nuovoGioco(){
@@ -20,20 +20,37 @@ void nuovoGioco(){
     printf("scegli un'opzione: ");    
     scanf("%d", &action);       // la scelta deve essere 1/2 (int)
 
-    if (action != 1 && action != 2) {
+    if ( action != 1 && action != 2 ) {
         printf("Scelta non valida, inserisci 1 o 2! \n");
         nuovoGioco();  // richiamo la funzione per far scegliere di nuovo
     }
-    else if (action == 1) {
+    else if ( action == 1 ) {
         // il nome del nuovo salvataggio è: giorno_mese_anno_ora_minuti
         time_t tempoNow = time(NULL);
         struct tm *infoTempo = localtime(&tempoNow);
         
         char nomeFile[80];
-        strftime(nomeFile, sizeof(nomeFile), "%d-%m-%Y_%H_%M:%S", infoTempo);
+        char percorsoFile[200];
+        char data[80];
+        strftime(nomeFile, sizeof(nomeFile), "%d-%m-%Y_%H-%M:%S", infoTempo);
+        sprintf(percorsoFile, "../salvataggi/%s.txt", nomeFile);
+        strftime(data, sizeof(data), "%d-%m-%Y %H-%M:%S", infoTempo);
         //printf("%s\n", nomeFile);
 
-        nuovoSalvataggio(nomeFile);
+        nuovoSalvataggio(percorsoFile, data);
+    }
+
+    else if ( action == 2 ){
+        int nSalvataggio;
+
+        listaSalvataggi();
+        printf("scegli un salvataggio da caricare: ");
+        scanf("%d", &nSalvataggio);
+        
+        printf("messaggio temporaneo, il codice esegue bene\n");
+        // TODO: CARICA UNA PARTITA
+        // TODO: implementa i trucchi
+    
     }
 
     return;
--- ELIA/nuovoSalvataggio.h
+++
@@ -1,6 +0,0 @@
-#ifndef SALVATAGGIO_H
-#define SALVATAGGIO_H
-
-void nuovoSalvataggio(char* time);
-
-#endif 
